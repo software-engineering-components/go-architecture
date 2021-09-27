6ccb2562c602ea9e18ea46da5285802f92516715
RV8V <rv.volovik88@gmail.com>
2021-09-27 22:50:38 +0300

    chor: fmt go

--- server.go
+++ server.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "time"
-  "net/http"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 type JsonTime struct {
@@ -12,14 +12,14 @@ type JsonTime struct {
 }
 
 func getCurrentTime(w http.ResponseWriter, r *http.Request) {
-  rfc3339 := time.Now().Format("2006-01-02 15:04:05.000")
+	rfc3339 := time.Now().Format("2006-01-02 15:04:05.000")
 
-  out, _ := json.Marshal(JsonTime{Time: rfc3339,})
+	out, _ := json.Marshal(JsonTime{Time: rfc3339})
 
-  fmt.Fprintf(w, string(out))
+	fmt.Fprintf(w, string(out))
 }
 
 func main() {
-  http.HandleFunc("/time", getCurrentTime)
-  http.ListenAndServe(":8795", nil)
+	http.HandleFunc("/time", getCurrentTime)
+	http.ListenAndServe(":8795", nil)
 }
