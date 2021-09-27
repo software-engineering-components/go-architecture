b6710133f8c96ca28784c9059d13327ab2bf7a19
RV8V <rv.volovik88@gmail.com>
2021-09-27 22:49:53 +0300

    feat: w as rfc3339 format

--- server.go
+++ server.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+  "encoding/json"
   "fmt"
+  "time"
   "net/http"
 )
 
-func getServerTime(w http.ResponseWriter, req *http.Request) {}
+type JsonTime struct {
+	Time string `json:"time"`
+}
+
+func getCurrentTime(w http.ResponseWriter, r *http.Request) {
+  rfc3339 := time.Now().Format("2006-01-02 15:04:05.000")
+
+  out, _ := json.Marshal(JsonTime{Time: rfc3339,})
+
+  fmt.Fprintf(w, string(out))
+}
 
 func main() {
-  http.HandleFunc("/time", getServerTime)
+  http.HandleFunc("/time", getCurrentTime)
   http.ListenAndServe(":8795", nil)
 }
