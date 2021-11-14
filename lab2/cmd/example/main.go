e971809fcbb009d8faadf30ef8423ab4e3e16f0b
RV8V <rv.volovik88@gmail.com>
2021-11-14 22:15:33 +0200

    fix: postfix to prefix

--- lab2/cmd/example/main.go
+++ lab2/cmd/example/main.go
@@ -1,27 +1,63 @@
 package main
 
 import (
-	"flag"
+	"bufio"
 	"fmt"
-	lab2 "github.com/roman-mazur/architecture-lab-2"
+	"os"
+	"strings"
+	repo "github.com/software-engineering-components/go-architecture/lab2"
 )
 
-var (
-	inputExpression = flag.String("e", "", "Expression to compute")
-	// TODO: Add other flags support for input and output configuration.
-)
+//
+// var (
+// 	inputExpression = flag.String("e", "", "Expression to compute")
+// 	// TODO: Add other flags support for input and output configuration.
+// )
+//
+// func main() {
+// 	flag.Parse()
+//
+// 	// TODO: Change this to accept input from the command line arguments as described in the task and
+// 	//       output the results using the ComputeHandler instance.
+// 	//       handler := &lab2.ComputeHandler{
+// 	//           Input: {construct io.Reader according the command line parameters},
+// 	//           Output: {construct io.Writer according the command line parameters},
+// 	//       }
+// 	//       err := handler.Compute()
+//
+// 	res, _ := lab2.PrefixToPostfix("+ 2 2")
+// 	fmt.Println(res)
+// }
+
+
+
+// package main
+//
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"os"
+// 	"strings"
+// )
+
+func ReadFromInput() (string, error) {
+
+	reader := bufio.NewReader(os.Stdin)
+	s, err := reader.ReadString('\n')
+
+	return strings.TrimSpace(s), err
+}
 
 func main() {
-	flag.Parse()
 
-	// TODO: Change this to accept input from the command line arguments as described in the task and
-	//       output the results using the ComputeHandler instance.
-	//       handler := &lab2.ComputeHandler{
-	//           Input: {construct io.Reader according the command line parameters},
-	//           Output: {construct io.Writer according the command line parameters},
-	//       }
-	//       err := handler.Compute()
+	fmt.Print("Enter infix expression: ")
+	infixString, err := ReadFromInput()
 
-	res, _ := lab2.PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+	if err != nil {
+		fmt.Println("Error when scanning input:", err.Error())
+		return
+	}
+
+	fmt.Println("Ya you postfix notation:", repo.lab2.ToPostfix(infixString))
+	return
 }
